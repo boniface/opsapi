1a3fbcf7e560a44b12e9c168ce1ac0485a4e0a54
Aiden <aidenpage@gmail.com>
2016-11-18 23:22:13 +0200

    update lot domain

--- app/domain/Lot.scala
+++ app/domain/Lot.scala
@@ -2,6 +2,7 @@ package domain
 
 import play.api.libs.json.Json
 
+
 /**
   * Created by aidenp on 2016/11/18.
   */
@@ -10,7 +11,7 @@ case class Lot (id: String,
                 description:String,
                 value:Value,
                 guarantee:Guarantee,
-                date:String,
+                date:String = Date,
                 minimalStep:Value,
                 auctionPeriod:Period,
                 auctionUrl:String,
