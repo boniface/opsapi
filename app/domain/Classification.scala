9aee8aa97b5a2c8c88e51eac5d0266b8ba03d9ad
Aiden <aidenpage@gmail.com>
2016-11-18 23:21:43 +0200

    format classification

--- app/domain/Classification.scala
+++ app/domain/Classification.scala
@@ -5,7 +5,10 @@ import play.api.libs.json.Json
 /**
   * Created by aidenp on 2016/11/18.
   */
-case class Classification (scheme:String, id:String, description:String, uri:String)
+case class Classification (scheme:String,
+                           id:String,
+                           description:String,
+                           uri:String)
 
 object Classification {
   implicit val classificationFmt = Json.format[Classification]
