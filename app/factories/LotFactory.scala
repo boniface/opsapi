aed496b6390f8533b08e7901e26b71d0f89d4a6e
Aiden <aidenpage@gmail.com>
2016-11-29 05:36:27 +0200

    update LotFactory

--- app/factories/LotFactory.scala
+++ app/factories/LotFactory.scala
@@ -1,10 +1,28 @@
 package factories
 
+import domain.{Guarantee, Lot}
+
 /**
   * Created by AidenP on 2016/11/23.
   */
 class LotFactory {
 
-  def create(values: Map(Str))
-
+  def createLot(values: Map[String,String],
+             value: Value,
+             //date: Date,
+             guarantee:Guarantee,
+             auctionPeriod: Period
+            ):Lot={
+                Lot(
+                  id = values("id"),
+                  title = values("title"),
+                  description = values("description"),
+                  value = value,
+                  guarantee = guarantee,
+                  //date = values("date") = date,
+                  minimalStep = value,
+                  auctionPeriod = auctionPeriod,
+                  auctionUrl = values("auctionUrl"),
+                  status = values("status"))
+            }
 }
