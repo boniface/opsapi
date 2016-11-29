7af7707ffa6ebcda576525b08c8711a04b66e4cd
Aiden <aidenpage@gmail.com>
2016-11-29 05:36:53 +0200

    update UnitFactory

--- app/factories/UnitFactory.scala
+++ app/factories/UnitFactory.scala
@@ -1,8 +1,14 @@
 package factories
 
+import domain.{Unit}
+
 /**
   * Created by AidenP on 2016/11/23.
   */
 class UnitFactory {
 
+  def createUnit(values: Map[String,String]):Unit={Unit(
+    code = values("code"),
+    name = values("name")
+  )}
 }
