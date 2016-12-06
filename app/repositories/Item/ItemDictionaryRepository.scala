2d99469f75153043da65a1e05a0f3d5434036420
Aiden <aidenpage@gmail.com>
2016-12-06 15:29:45 +0200

    update ItemDictionaryRepository

--- app/repositories/Item/ItemDictionaryRepository.scala
+++ app/repositories/Item/ItemDictionaryRepository.scala
@@ -1,10 +1,58 @@
 package repositories.Item
 
+import Item.ItemDictionary
+import com.datastax.driver.core.Row
+import com.websudos.phantom.CassandraTable
+import com.websudos.phantom.column.PrimitiveColumn
+import com.websudos.phantom.dsl._
+import com.websudos.phantom.keys.PartitionKey
+import com.websudos.phantom.reactivestreams._
+import conf.connection.DataConnection
+
+import scala.concurrent.Future
+
 /**
   * Created by AidenP on 2016/11/30.
   */
-class ItemDictionaryRepository {
+class ItemDictionaryRepository extends CassandraTable[ItemDictionaryRepository, ItemDictionary]{
+  object ItemId extends StringColumn(this) with PrimaryKey[String]
+  object DictionaryId extends  StringColumn(this) with PartitionKey[String]
 
-  ItemId:String,
-  DictionaryId:String
+  override def fromRow(r: Row):ItemDictionary = {
+    ItemDictionary(
+      ItemId(r),
+      DictionaryId(r))
+  }
+
+}
+
+object ItemDictionaryRepository extends ItemDictionaryRepository with RootConnector {
+  override lazy val tableName = "schedule"
+
+  override implicit def space: KeySpace = DataConnection.keySpace
+
+  override implicit def session: Session = DataConnection.session
+
+  def save(itemDictionary: ItemDictionary): Future[ResultSet] = {
+    insert
+      .value(_.ItemId, itemDictionary.ItemId)
+      .value(_.DictionaryId, itemDictionary.DictionaryId)
+      .future()
+  }
+
+  def getItemClassificationById(ItemId: String, DictionaryId: String): Future[Option[ItemDictionary]] = {
+    select.where(_.ItemId eqs ItemId). and(_.DictionaryId eqs DictionaryId).one()
+  }
+
+  def getAllItemClassifications: Future[Seq[ItemDictionary]] = {
+    select.fetchEnumerator() run Iteratee.collect()
+  }
+
+  def getItemClassification(ItemId: String, DictionaryId: String): Future[Seq[ItemDictionary]] = {
+    select.where(_.ItemId eqs ItemId). and(_.DictionaryId eqs DictionaryId).fetchEnumerator() run Iteratee.collect()
+  }
+
+  def deleteById(ItemId: String, DictionaryId: String): Future[ResultSet] = {
+    delete.where(_.ItemId eqs ItemId). and(_.DictionaryId eqs DictionaryId).future()
+  }
 }
