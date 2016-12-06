1b686146763eb1bf788ce56724b652d701959f43
Aiden <aidenpage@gmail.com>
2016-12-06 15:30:03 +0200

    update ItemLotRepository

--- app/repositories/Item/ItemLotRepository.scala
+++ app/repositories/Item/ItemLotRepository.scala
@@ -1,10 +1,61 @@
 package repositories.Item
 
+import com.datastax.driver.core.Row
+import com.websudos.phantom.CassandraTable
+import com.websudos.phantom.column.PrimitiveColumn
+import com.websudos.phantom.dsl._
+import com.websudos.phantom.keys.PartitionKey
+import com.websudos.phantom.reactivestreams._
+import conf.connection.DataConnection
+import domain.Item.ItemLot
+
+import scala.concurrent.Future
+
 /**
   * Created by AidenP on 2016/11/30.
   */
-class ItemLotRepository {
+class ItemLotRepository  extends CassandraTable[ItemLotRepository, ItemLot]{
+  object ItemId extends StringColumn(this) with PrimaryKey[String]
+  object LotId extends  StringColumn(this) with PartitionKey[String]
 
-  ItemId:String,
-  LotId:String
+  override def fromRow(r: Row):ItemLot = {
+    ItemLot(
+      ItemId(r),
+      LotId(r))
+  }
+
 }
+
+object ItemLotRepository extends ItemLotRepository with RootConnector {
+  override lazy val tableName = "schedule"
+
+  override implicit def space: KeySpace = DataConnection.keySpace
+
+  override implicit def session: Session = DataConnection.session
+
+  def save(itemLot: ItemLot): Future[ResultSet] = {
+    insert
+      .value(_.ItemId, itemLot.ItemId)
+      .value(_.LotId, itemLot.LotId)
+      .future()
+  }
+
+  def getItemClassificationById(ItemId: String, LotId: String): Future[Option[ItemLot]] = {
+    select.where(_.ItemId eqs ItemId). and(_.LotId eqs LotId).one()
+  }
+
+  def getAllItemClassifications: Future[Seq[ItemLot]] = {
+    select.fetchEnumerator() run Iteratee.collect()
+  }
+
+  def getItemClassification(ItemId: String, LotId: String): Future[Seq[ItemLot]] = {
+    select.where(_.ItemId eqs ItemId). and(_.LotId eqs LotId).fetchEnumerator() run Iteratee.collect()
+  }
+
+  def deleteById(ItemId: String, LotId: String): Future[ResultSet] = {
+    delete.where(_.ItemId eqs ItemId). and(_.LotId eqs LotId).future()
+  }
+}
+    ItemId:String,
+  LotId:String
+
