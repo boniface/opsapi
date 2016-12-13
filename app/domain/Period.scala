66634402ee245106ca2830bfc2136e558c859546
Ryan-Carstens <rcstringers@gmail.com>
2016-12-13 12:20:54 +0200

    update 1.0 - factories

--- app/domain/Period.scala
+++ app/domain/Period.scala
@@ -7,8 +7,8 @@ import scala.util.Random
 /**
   * Created by sanXion on 2016/11/24.
   */
-case class Period (startDate : String = Date,
-                   endDate : String = Date)
+case class Period (startDate : String,
+                   endDate : String)
 
 object Period{
   implicit val periodFmt = Json.format[Period]
---
+++ app/factories/ContractFactory.scala
@@ -0,0 +1,25 @@
+package factories
+
+import domain.{Contract, Period, Value}
+
+/**
+  * Created by sanXion on 2016/12/11.
+  */
+class ContractFactory {
+  def createContract(values:Map[String, String], period : Period, value : Value , item :List[Item], organization :List[Organization], document :List[Document]):Contract={
+    Contract(id = values("id"),
+              awardID = values("awardID"),
+              contractID = values("contractID"),
+              contractNumber = values("contractNumber"),
+              title = values("title"),
+              description = values("description"),
+              value = value,
+              items = item,
+              suppliers = organization,
+              status = values("status"),
+              period = period,
+              dateSigned = values("dateSigned"),
+              date = values("date"),
+              documents = document)
+  }
+}
