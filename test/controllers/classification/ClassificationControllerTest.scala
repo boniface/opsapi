a1e359bb85de288a0c191737ea4bffd6d9f03801
Aiden <aidenpage@gmail.com>
2016-12-14 15:51:23 +0200

    Update classification controller test

--- test/controllers/classification/ClassificationControllerTest.scala
+++ test/controllers/classification/ClassificationControllerTest.scala
@@ -1,8 +1,46 @@
 package controllers.classification
 
+import domain.Classification.Classification
+import org.joda.time.DateTime
+import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
 /**
   * Created by AidenP on 2016/12/13.
   */
-class ClassificationControllerTest {
+class ClassificationControllerTest extends PlaySpec with OneAppPerTest {
+  val classification = Classification(
+    "test",
+    "1",
+    "test",
+    "test")
 
+  "Routes" should {
+
+
+
+    "ClassificationController" should {
+
+      "Create Classification Object in Through Controller" in {
+        val request =  route(app, FakeRequest(POST, "/classification/create")
+          .withJsonBody(Json.toJson(classification)))
+          .get
+        status(request) mustBe OK
+        contentType(request) mustBe Some("application/json")
+        println(" The Content is ", contentAsString(request))
+      }
+
+      "Get Mail From Controller" in {
+        val request = route(app, FakeRequest(GET, "/classification/test")
+        ).get
+        status(request) mustBe OK
+        contentType(request) mustBe Some("application/json")
+        println(" The Output", contentAsJson(request))
+      }
+
+
+    }
+  }
 }
