fe4cc7b0662de34eacaf8c4d72bf2cc6f783be46
Bonakele Lesibane <bonakele@skywalkinnovations.co.za>
2016-12-18 23:49:39 +0200

    Work done: 	- added some more tests

--- test/repositories/CancellationRepositoryTest.scala
+++ test/repositories/CancellationRepositoryTest.scala
@@ -1,34 +1,38 @@
 package repositories
 
+import domain.Cancellation
 import org.scalatest.{FeatureSpec, GivenWhenThen}
+import repositories.CancellationRepository
+
+import scala.concurrent.Await
 
 /**
   * Created by Mzuvukile Lawana on 2016/12/12.
   */
 class CancellationRepositoryTest extends FeatureSpec with GivenWhenThen {
 
-//  feature("Create cancellation") {
-//    info("Add an cancellation")
-//    scenario("Add new cancellation ") {
-//      Given("Given cancellationId,reason,date,status,document,cancellationOf,relatedLot")
-//      val cancellationId = "12345"
-//      val reason = "Nothing"
-//      val date = "26/18/12"
-//      val status = "Done"
-//      val documents = "Documnets"
-//      val cancellationOf = "Bond"
-//      val relatedLot = "related"
-//
-//      Then("Add cancellation ")
-//      val admin = Abuse(siteId,subjectId,commentIdOrResponseId,abuseId,details,emailId,date)
-//      val abuseRepo = AbuseRepository
-//      abuseRepo.save(admin)
-//      Then("Display All ")
-//      val displayAllstatuses = Await.result(abuseRepo.getSiteAbuse(siteId), 2 minutes)
-//      displayAllstatuses.foreach(i => println("Abuse=======>", i))
-//      val displayIdStatus = Await.result(abuseRepo.getAbuseBySubjectId(siteId,subjectId), 2 minutes)
-//      displayIdStatus.foreach(i => println("AdminStatus=======>", i))
-//    }
-//  }
+  feature("Create cancellation") {
+    info("Add an cancellation")
+    scenario("Add new cancellation ") {
+      Given("Given cancellationId,reason,date,status,document,cancellationOf,relatedLot")
+      val cancellationId = "12345"
+      val reason = "Nothing"
+      val date = "26/18/12"
+      val status = "Done"
+      val documents = "Documnets"
+      val cancellationOf = "Bond"
+      val relatedLot = "related"
+
+      Then("Add cancellation ")
+      val cancellation = Cancellation(cancellationId,reason,date,status,documents,cancellationOf,cancellationOf,relatedLot)
+      val cancellationRepo = CancellationRepository
+      cancellationRepo.save(cancellation)
+      Then("Display All ")
+      val displayAllcancelled = Await.result(cancellationRepo.getCancellationById(cancellationId), 2 minutes)
+      displayAllcancelled.foreach(i => println("Cancellation=======>", i))
+      val displayIdStatus = Await.result(cancellationRepo.getCancellationById(cancellationId), 2 minutes)
+      displayIdStatus.foreach(i => println("CancellationStatus=======>", i))
+    }
+  }
 
 }
